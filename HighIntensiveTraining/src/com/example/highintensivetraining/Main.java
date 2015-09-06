9b404d7ac04778f47a259d0c0089853721aeb288
Medium <asd@aw.com>
2015-09-06 04:18:56 +0200

    Adding onClick listener and trying to find out which day is klicked

--- HighIntensiveTraining/src/com/example/highintensivetraining/Main.java
+++ HighIntensiveTraining/src/com/example/highintensivetraining/Main.java
@@ -2,10 +2,10 @@ package com.example.highintensivetraining;
 
 import android.support.v7.app.ActionBarActivity;
 
-import com.hit.calendarview.HitCalendarView;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -14,21 +14,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewParent;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class Main extends Activity implements OnDateChangeListener {
+public class Main extends Activity {
 
-	HitCalendarView calendar;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		//GridLayout month = (GridLayout) findViewById(R.id.month);
+		//month.setOnClickListener((android.view.View.OnClickListener) this);
+		//month.setOnClickListener((android.view.View.OnClickListener) this);
 	}
 	
 	@Override
@@ -50,9 +56,28 @@ public class Main extends Activity implements OnDateChangeListener {
 		return super.onOptionsItemSelected(item);
 	}
 
-
-	@Override
-	public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		/*
+		String activities[] = { "day1" };
+		
+		int id = v.getId();
+		for (int i = 0; i < activities.length; i++) {
+			if (id == getResources().getIdentifier("day" + i, "id",
+					getPackageName())) {
+				Toast.makeText(getApplicationContext(), i , 
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+		*/
+		
+		int j = getResources().getIdentifier("day1", "id",
+				getPackageName());
+		int p = v.getId();
+		
+		Toast.makeText(getApplicationContext(), ((Integer) p).toString() +
+				"und j ist" + ((Integer) j).toString() , 
+				Toast.LENGTH_SHORT).show();
+		
 	}
 }
